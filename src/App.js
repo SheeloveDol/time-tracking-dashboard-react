ebf852debc5cb87b4b4323b4a2bc7e4625d6a256
Sheelove Dol <siesel26@gmail.com>
2022-07-26 21:37:51 -0700

    figured out how to do the cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div>
-      Hello me!
-    </div>
+    <Box  borderRadius='xl' m='auto' border='1px' bg='blue.400' borderColor='tomato.200' w='80%' h='500px'>
+      <Box border='1px' borderColor='red.400' mt='2rem' bg='yellow.200' h='475px' borderRadius='xl'>
+        Hello me!
+      </Box>
+    </Box>
   );
 }
 
