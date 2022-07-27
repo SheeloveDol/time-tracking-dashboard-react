0c7029fce002da6cba9cfc74decde3e2d2623978
Sheelove Dol <siesel26@gmail.com>
2022-07-26 21:54:45 -0700

    Adjusted the size of the card

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Box  borderRadius='xl' m='auto' border='1px' bg='blue.400' borderColor='tomato.200' w='80%' h='500px'>
-      <Box border='1px' borderColor='red.400' mt='2rem' bg='yellow.200' h='475px' borderRadius='xl'>
+    <Box  borderRadius='xl' m='auto' border='1px' bg='blue.400' borderColor='tomato.200' w='80%' h='250px'>
+      <Box border='1px' borderColor='red.400' mt='2rem' bg='yellow.200' h='216px' borderRadius='xl'>
         Hello me!
       </Box>
     </Box>
