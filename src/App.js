7016823d6744eb534810e84ade9bfed312032609
Sheelove Dol <siesel26@gmail.com>
2022-07-28 21:28:31 -0700

    added flex for cards title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import { Box, SimpleGrid, Flex, Heading, Text } from "@chakra-ui/react";
 import CardList from "./components/cardList/CardList.component";
 import Card from "./components/card/Card.component";
 import HeaderCard from "./components/headerCard/HeaderCard.component";
@@ -24,7 +24,15 @@ function App() {
             borderColor='tomato.200' w='100%'
              h='160px'>
                 <Box mt='40px' w='100%' bg={item.cardBgColor} h='120px' borderRadius='lg' >
-                    
+                  <Flex p='.5rem' w='80%' color='white' m='auto' mt='' 
+                  justify='space-between'>
+                    <Text  fontSize='sm'>{item.title}
+                    </Text>
+                    <Box >
+                      <Text fontSize='lg'>...</Text>
+                    </Box>
+                  </Flex>
+                  <Flex></Flex>
                 </Box>
         </Box>
             )
