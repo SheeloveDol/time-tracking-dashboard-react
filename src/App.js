f170e368b614185b3004a8b44eb8a6108330e263
Sheelove Dol <siesel26@gmail.com>
2022-07-29 21:08:31 -0700

    Fixed card dimensions

--- src/App.js
+++ src/App.js
@@ -25,10 +25,10 @@ function App() {
               <Box key={item.id} borderRadius='xl' m='auto'
                 bg={item.accentBgColor}
                 borderColor='tomato.200' w='100%'
-                h='160px'>
+                h='239px'>
 
                 <Box mt='40px' w='100%' bg={item.cardBgColor}
-                    h='120px' borderRadius='lg' >
+                    h='199px' borderRadius='lg' >
 
                   <Flex p='.5rem' w='80%' color='white' m='auto' 
                   justify='space-between'>
@@ -39,8 +39,8 @@ function App() {
                     </Box>
                   </Flex>
 
-                  <Flex border='1px' color='white'justify='space-between' direction={['row', 'column']}>
-                    <Text fontSize='5xl'>{item.timeframes.weekly.current}</Text>
+                  <Flex border='1px' color='white' justify='space-between' direction={['row', 'column']}>
+                    <Text fontSize='6xl'>{item.timeframes.weekly.current}hrs</Text>
                     <Box border='1px'>Last Week - {item.timeframes.weekly.previous}</Box>
                   </Flex>
 
