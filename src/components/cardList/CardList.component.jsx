e4950b7922cd6cdd247fd3293f7611ba317e4710
Sheelove Dol <siesel26@gmail.com>
2022-08-02 21:22:08 -0700

    Still trying to figure out the state

--- src/components/cardList/CardList.component.jsx
+++ src/components/cardList/CardList.component.jsx
@@ -1,18 +1,27 @@
 import { Box, Text, Flex, SimpleGrid, Button } from "@chakra-ui/react";
 import HeaderCard from "../headerCard/HeaderCard.component";
+import { useState } from "react";
 
-const CardList = ({cardsData}) => {
+const CardList = ({cardsData}) => { 
 
-    const  monthly = false;
-    const daily = false;
-    const weekly = true;
+    const [period, setPeriod] = useState({
+        monthly: false,
+        weekly: true,
+        daily: false
+
+    })
+
+    // const  monthly = false;
+    // const daily = false;
+    // const weekly = true;
 
     return (
         <Flex border='1px' p='1rem' columns={[1, 2]} minWidth={['374px','1094px']} direction={['column', 'row']} gap='1rem' justify-content='space-between' bgColor='hsl(226, 43%, 10%)'>
         
             <HeaderCard />
-            
+
             <SimpleGrid  columns={[1, 3]} w='100%' gridGap='30px'>
+                <Button onClick={(prev) => setPeriod() }>click me</Button>
                 {
                     cardsData.map( item => 
                         <Box key={item.id} borderRadius='xl' m='auto'bg={item.accentBgColor} borderColor='tomato.200' 
@@ -30,14 +39,14 @@ const CardList = ({cardsData}) => {
                                     
                                 <Flex border='1px' color='white' justify='space-between' direction={['row', 'column']}>
                                     <Text fontSize='6xl'>
-                                        {monthly && item.timeframes.monthly.current} 
-                                        {weekly && item.timeframes.weekly.current} 
-                                        {daily && item.timeframes.daily}hrs
+                                        {period.monthly && item.timeframes.monthly.current} 
+                                        {period.weekly && item.timeframes.weekly.current} 
+                                        {period.daily && item.timeframes.daily}hrs
                                     </Text>
                                     <Box border='1px'>
-                                        Last Week - {monthly && item.timeframes.monthly.previous}
-                                        {weekly && item.timeframes.monthly.previous}
-                                        {daily && item.timeframes.monthly.previous}
+                                        Last Week - {period.monthly && item.timeframes.monthly.previous}
+                                        {period.weekly && item.timeframes.monthly.previous}
+                                        {period.daily && item.timeframes.monthly.previous}
                                     </Box>
                                 </Flex>  
                             </Box>
