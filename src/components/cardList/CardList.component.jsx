536193efb6967322644b587b0ebc975b2ea04429
Sheelove Dol <siesel26@gmail.com>
2022-08-05 23:03:44 -0700

    created assets folder and now working on headercard component

--- src/components/cardList/CardList.component.jsx
+++ src/components/cardList/CardList.component.jsx
@@ -4,16 +4,7 @@ import { useState } from "react";
 
 const CardList = ({cardsData}) => { 
 
-    const [period, setPeriod] = useState({
-        monthly: false,
-        weekly: true,
-        daily: false
-
-    })
-
-    // const  monthly = false;
-    // const daily = false;
-    // const weekly = true;
+    const [period, setPeriod] = useState('weekly');
 
     return (
         <Flex border='1px' p='1rem' columns={[1, 2]} minWidth={['374px','1094px']} direction={['column', 'row']} gap='1rem' justify-content='space-between' bgColor='hsl(226, 43%, 10%)'>
@@ -21,7 +12,6 @@ const CardList = ({cardsData}) => {
             <HeaderCard />
 
             <SimpleGrid  columns={[1, 3]} w='100%' gridGap='30px'>
-                <Button onClick={(prev) => setPeriod() }>click me</Button>
                 {
                     cardsData.map( item => 
                         <Box key={item.id} borderRadius='xl' m='auto'bg={item.accentBgColor} borderColor='tomato.200' 
@@ -39,14 +29,16 @@ const CardList = ({cardsData}) => {
                                     
                                 <Flex border='1px' color='white' justify='space-between' direction={['row', 'column']}>
                                     <Text fontSize='6xl'>
-                                        {period.monthly && item.timeframes.monthly.current} 
-                                        {period.weekly && item.timeframes.weekly.current} 
-                                        {period.daily && item.timeframes.daily}hrs
+                                        {period === 'monthly' && item.timeframes.monthly.current} 
+                                        {period === 'weekly' && item.timeframes.weekly.current} 
+                                        {period === 'daily' && item.timeframes.daily}hrs
                                     </Text>
+
                                     <Box border='1px'>
-                                        Last Week - {period.monthly && item.timeframes.monthly.previous}
-                                        {period.weekly && item.timeframes.monthly.previous}
-                                        {period.daily && item.timeframes.monthly.previous}
+                                        {period === 'monthly' && <Text>Last month - {item.timeframes.monthly.previous}</Text>}
+                                        
+                                        {period === 'weekly' && <Text>Last week - {item.timeframes.monthly.previous}</Text>}
+                                        {period === 'daily' && <Text>Last day - {item.timeframes.monthly.previous}</Text>}
                                     </Box>
                                 </Flex>  
                             </Box>
--- src/components/headerCard/HeaderCard.component.jsx
+++ src/components/headerCard/HeaderCard.component.jsx
@@ -1,15 +1,27 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Flex, Button } from "@chakra-ui/react";
 
 const HeaderCard = () => {
     return (
-        <Box border='1px' w={['100%', '300px']}>
-            <Box p='2rem' bgColor='blue.400'></Box>
-            <Box p='1rem' bgColor='purple.300'>
+        <Flex 
+            border='1px' 
+            w={['100%', '255px']} 
+            h={['203px', '518px']}
+            direction='column'
+            >
+            <Box 
+                p='2rem' 
+                bgColor='blue.400'
+                flex='2'
+                >
+
+            </Box>
+            <Box p='1rem' bgColor='purple.300'
+            flex='1'>
                 <Button>Daily</Button>
                 <Button>Weekly</Button>
                 <Button>Monthly</Button>
             </Box>
-        </Box>
+        </Flex>
     )
 }
 
